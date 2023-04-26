b5fe13536aa6ae2bd5e466428ef734f7cabeaede
Nagendravarma1 <nagendravarmal512@gmail.com>
2023-04-26 16:05:12 +0530

    Base Project

---
+++ App.js
@@ -0,0 +1,21 @@
+import React,{useState} from "react";
+import AddUser from "./Components/User/AddUser";
+import UserList from "./Components/User/UserList";
+
+
+const App = () => {
+  const [usersList, setUsersList] = useState([])
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return[...prevUsersList, {name: uName, age: uAge, id: Math.random().toString()}];
+    })
+  }
+  return (
+    <React.Fragment>
+      <AddUser onAddUser={addUserHandler}/>
+      <UserList users={usersList}/>
+    </React.Fragment>
+  )
+}
+export default App;
